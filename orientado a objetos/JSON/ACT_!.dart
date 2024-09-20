516d588cd087d4f26c75c70b2cc51e8dc2878445
julianmeza33 <julianmezaandrestorres20@gmail.com>
2024-09-20 17:23:35 -0500

    Update ACT_!.dart

--- orientado a objetos/JSON/ACT_!.dart
+++ orientado a objetos/JSON/ACT_!.dart
@@ -1,8 +1,25 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-void main()async{
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/posts/1');
+class User{
+  int? userld;
+  int? id;
+  String? title;
+  String? body;
+  
+  User(String m){
+    Map<String, dynamic> map = jsonDecode(m);
+    this.userld = map[];
+    this.id = map;
+    this.title = map;
+    this.body = map;
+    
+  }
+}
+
+void main() async {
+  var url = Uri.https('jsonplaceholder.typicode.com', '/posts/1');
   var response = await http.get(url);
-print('Response status: ${response.statusCode}');
-print('Response body: ${response.body}');
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 }
