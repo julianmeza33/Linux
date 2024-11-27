649e39f4200589ad38dc2d349caadc89311a6809
julianmeza33 <julianmezaandrestorres20@gmail.com>
2024-11-27 15:09:24 -0500

    nb

---
+++ html/prueva/script.js
@@ -0,0 +1,8 @@
+var n = document.querySelector("body > span:nth-child(2)")
+n.innerHTML = "julian"
+var d = document.querySelector("body > span:nth-child(5)")
+d.innerHTML = "18a 4-51"
+var t = document.querySelector("body > span:nth-child(8)")
+t.innerHTML = "3244107629"
+var e = document.querySelector("body > span:nth-child(11)")
+e.innerHTML = "18"
