90f6c3041438918c9915767d44478671fb7f8470
julianmeza33 <julianmezaandrestorres20@gmail.com>
2024-11-27 16:54:17 -0500

    d

--- html/prueva/script.js
+++ html/prueva/script.js
@@ -1,8 +1,15 @@
-var n = document.querySelector("body > span:nth-child(2)")
-n.innerHTML = "julian"
-var d = document.querySelector("body > span:nth-child(5)")
-d.innerHTML = "18a 4-51"
-var t = document.querySelector("body > span:nth-child(8)")
-t.innerHTML = "3244107629"
-var e = document.querySelector("body > span:nth-child(11)")
-e.innerHTML = "18"
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json =>{      
+        var a = document.querySelector("body > main > table > thead:nth-child(1) > tr > td")
+        a.innerHTML = json.userId
+
+        var b = document.querySelector("body > main > table > thead:nth-child(2) > tr > td")
+        b.innerHTML = json.id
+
+        var c = document.querySelector("body > main > table > thead:nth-child(3) > tr > td")
+        c.innerHTML = json.title
+
+        var d = document.querySelector("body > main > table > thead:nth-child(4) > tr > td")
+        d.innerHTML = json.completed
+      })
