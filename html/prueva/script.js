04f3232ed2613e1fb26da292c2f337f5dbe31194
julianmeza33 <julianmezaandrestorres20@gmail.com>
2025-01-02 21:37:01 -0500

    listo

--- html/prueva/script.js
+++ html/prueva/script.js
@@ -1,21 +1,35 @@
-const boton = document.getElementById('miBoton');
-const barraBusqueda = document.getElementById('barraBusqueda');
+const boton = document.getElementById("busqueda");
+const barra = document.getElementById("barra");
 
 boton.addEventListener('click', () => {
-    boton.style.backgroundColor = 'red';
+    boton.style.backgroundColor = '#34ac44';
     boton.style.color = 'white';
-    
-    const dato = barraBusqueda.value;
+
+    const dato = barra.value;
     console.log(dato);
 
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => {
-            // Mostrar los datos en la tabla
-            var a = document.querySelector("body > main > table > tbody > tr > td:nth-child(1)");
-            if (!a) {
-                a = document.createElement('tr');
-                document.querySelector("body > main > table > tbody").appendChild(a);
+    fetch(`https://jsonplaceholder.typicode.com/todos/${dato}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Datos no encontrados');
             }
-            a.innerHTML = json.userId;
-})})
+            return response.json();
+        })
+        .then(json => {
+            var a = document.querySelector("body > main > table > thead:nth-child(1) > tr > td");
+            a.textContent = json.userId;
+
+            var b = document.querySelector("body > main > table > thead:nth-child(2) > tr > td");
+            b.textContent = json.id;
+
+            var c = document.querySelector("body > main > table > thead:nth-child(3) > tr > td");
+            c.textContent = json.title;
+
+            var d = document.querySelector("body > main > table > thead:nth-child(4) > tr > td");
+            d.textContent = json.completed;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('Datos no encontrados');
+        });
+});
